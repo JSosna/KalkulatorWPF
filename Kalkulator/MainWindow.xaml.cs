2afb0c9038d39f915ec35d388dacad765958657f
SosnaK1 <jakub.sosna@icloud.com>
2020-03-04 01:31:50 +0100

    Add event handlers to buttons

--- Kalkulator/MainWindow.xaml.cs
+++ Kalkulator/MainWindow.xaml.cs
@@ -20,9 +20,97 @@ namespace Kalkulator
     /// </summary>
     public partial class MainWindow : Window
     {
+        //private bool lastButtonWasOperator;
+        private bool lastButtonWasOperator;
+        private Button lastOperator;
+        private List<double> savedNumbers = new List<double>();
+        private List<char> savedOperators = new List<char>();
+        private double finalResult = 0;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Number_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+
+            if (button.Content.ToString() == "," && !Wynik.Text.ToString().Contains(','))
+                Wynik.Text += ',';
+
+            else if (button.Content.ToString() != ",")
+            {
+                double tmp = 0;
+
+                if (lastButtonWasOperator)
+                {
+                    Double.TryParse(Wynik.Text, out tmp);
+                    savedNumbers.Add(tmp);
+
+                    Wynik.Text = "0";
+
+                    lastOperator.BorderThickness = new Thickness(0);
+                }
+
+                Double.TryParse(Wynik.Text + button.Content, out tmp);
+                Wynik.Text = $"{tmp}";
+            }
+
+            lastButtonWasOperator = false;
+        }
+
+        private void Operator_Click(object sender, RoutedEventArgs e)
+        {
+            if (lastButtonWasOperator)
+            {
+                lastOperator.BorderThickness = new Thickness(0);
+                savedOperators.RemoveAt(savedOperators.Count - 1);
+            }
+                
+
+            if (lastOperator != sender || !lastButtonWasOperator)
+            {
+                lastButtonWasOperator = true;
+                lastOperator = sender as Button;
+                lastOperator.BorderThickness = new Thickness(2);
+                savedOperators.Add(lastOperator.Content.ToString()[0]);
+            }
+            else
+            {
+                // Dzięki temu można odznaczyć operator i dalej wprowadzać liczbę
+                lastOperator = null;
+                lastButtonWasOperator = false;
+                savedOperators.RemoveAt(savedOperators.Count - 1);
+            }
+                
+        }
+
+        private void AC_Click(object sender, RoutedEventArgs e)
+        {
+            savedNumbers.Clear();
+            finalResult = 0;
+            Wynik.Text = "0";
+            lastButtonWasOperator = false;
+            lastOperator.BorderThickness = new Thickness(0);
+            lastOperator = null;
+        }
+
+        private void CE_Click(object sender, RoutedEventArgs e)
+        {
+            Wynik.Text = "0";
+            lastButtonWasOperator = false;
+            lastOperator.BorderThickness = new Thickness(0);
+            lastOperator = null;
+        }
+
+        private void Neg_Click(object sender, RoutedEventArgs e)
+        {
+            if (Wynik.Text.Contains("-"))
+                Wynik.Text = Wynik.Text.Replace("-", "");
+            else
+                Wynik.Text = "-" + Wynik.Text;
+
         }
     }
 }
